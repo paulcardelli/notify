49342d714165cce68a6e69c95f3d403373cfe541
tleegaard <tleegaard@gmail.com>
2013-09-25 21:48:46 +0200

    Added locale/gettext stuff to the notifier module.

--- notify_view.php
+++ notify_view.php
@@ -3,17 +3,17 @@
 
 <br>
 
-<h2>Notify me on inactive feeds</h2>
-<p>Receive an email notification when any feed become inactive for more than 2 hours</p>
+<h2><?php echo _('Notify me on inactive feeds'); ?></h2>
+<p><?php echo _('Receive an email notification when any feed become inactive for more than 2 hours'); ?></p>
 
 <div class="input-prepend input-append">
-  <span class="add-on">Send email to:</span>
+  <span class="add-on"><?php echo _('Send email to:'); ?></span>
   <input id="email" type="text" />
-  <span id="enabledtag" class="add-on" style="display:none">Enabled!</span>
-  <button id="notifybtn" class="btn btn-primary" type="button">Enable</button>
+  <span id="enabledtag" class="add-on" style="display:none"><?php echo _('Enabled!'); ?></span>
+  <button id="notifybtn" class="btn btn-primary" type="button"><?php echo _('Enable'); ?></button>
 </div>
 
-<p><i>The server checks for inactive feeds every 2 hours.<br>Feeds that are historically inactive are ignored.</i></p>
+<p><i><?php echo _('The server checks for inactive feeds every 2 hours.<br>Feeds that are historically inactive are ignored.'); ?></i></p>
 
 <script>
   var path = "<?php echo $path; ?>";
@@ -29,16 +29,16 @@
   // Enable / disable button
   $("#notifybtn").click(function(){
     enabled = !enabled;
-    console.log("Enabled: "+enabled);
+    console.log("<?php echo _('Enabled: '); ?>"+enabled);
     
     
     if (enabled) {
       var lastemail = email;
       email = $("#email").val();
-      console.log("Sending email to: "+email);
+      console.log("<?php echo _('Sending email to: '); ?>"+email);
       var result = notify.enable(email);
       if (result.success == false) {
-        alert('ERROR: Please enter a valid email address');
+        alert('<?php echo _('ERROR: Please enter a valid email address'); ?>');
         email = lastemail;
         enabled = false;
         update();
@@ -54,12 +54,12 @@
   {
     if (enabled) {
       $("#notifybtn").removeClass("btn-primary");
-      $("#notifybtn").html("Disable");
+      $("#notifybtn").html("<?php echo _('Disable'); ?>");
       $("#notifybtn").addClass("btn-danger");
       $("#enabledtag").show();
     } else {
       $("#notifybtn").removeClass("btn-danger");
-      $("#notifybtn").html("Enable");
+      $("#notifybtn").html("<?php echo _('Enable'); ?>");
       $("#notifybtn").addClass("btn-primary");
       $("#enabledtag").hide();
     }
