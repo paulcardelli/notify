007fbfd19c45aede09d3a64152e7de4721ea5984
paulcardelli <paul.cardelli@gmail.com>
2016-10-07 08:50:24 -0700

    added , 'order' => 10 for menu errors
    see topic https://openenergymonitor.org/emon/node/11376

--- notify_menu.php
+++ notify_menu.php
@@ -4,6 +4,6 @@
   bindtextdomain($domain, "Modules/user/locale");
   bind_textdomain_codeset($domain, 'UTF-8');
 
-  $menu_dropdown[] = array('name'=> dgettext($domain, _("Notify")), 'path'=>"notify" , 'session'=>"write");
+  $menu_dropdown[] = array('name'=> dgettext($domain, _("Notify")), 'path'=>"notify" , 'session'=>"write", 'order' => 10);
 
 ?>
